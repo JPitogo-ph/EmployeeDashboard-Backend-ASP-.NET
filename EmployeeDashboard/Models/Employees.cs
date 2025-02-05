ce0ea5f4f8cddc9d4ed52ffd769b9ed91bbcd975
james <jpitogodev@gmail.com>
2025-02-05 17:48:36 +0800

    Added attributes for string length and validation of non-nullable fields.

--- EmployeeDashboard/Models/Employees.cs
+++ EmployeeDashboard/Models/Employees.cs
@@ -1,17 +1,40 @@
-﻿namespace EmployeeDashboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeDashboard.Models
 {
     public class Employee
     {
+        [Required] 
         public int EmployeeId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber{ get; set; }
+        
+        [StringLength(32)]
+        [Required] 
+        public required string FirstName { get; set; }
+        
+        [StringLength(16)]
+        [Required] 
+        public required string LastName { get; set; }
+        
+        [StringLength(32)]
+        [Required] 
+        public required string Email { get; set; }
+        
+        [StringLength(16)]
+        [Required] 
+        public required string PhoneNumber{ get; set; }
+        
         public DateTime? HireDate { get; set; }
-        public string? JobId { get; set; }
+        
+        [StringLength(8)]
+        [Required] 
+        public required string JobId { get; set; }
+        
         public int Salary {  get; set; }
+        
         public Decimal CommissionPct { get; set; }
+        
         public int ManagerId { get; set; }
+        
         public int DepartmentId { get; set; }
 
     }
